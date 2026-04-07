558e369034057fec12de1a66c9326f6b77cc73fb
Muroslav11 <Muroslavhalik@gmail.com>
2026-04-07 20:49:45 +0300

    Lab11.1

---
+++ Lab11.1/Lab11.1/Lab11.1.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <fstream>
+#include <time.h>
+
+using namespace std;
+
+// Функція для створення початкового бінарного файлу з випадковими числами
+void Create(const char* fName)
+{
+    srand((unsigned)time(NULL));
+    ofstream f(fName, ios::binary);
+    int x;
+    int n;
+    cout << "Enter count of numbers: "; cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        x = -50 + rand() % 101; // генеруємо числа в діапазоні [-50; 50]
+        f.write((char*)&x, sizeof(x));
+    }
+    f.close();
+}
+
+// Функція для виведення вмісту бінарного файлу на екран
+void Print(const char* fName)
+{
+    ifstream f(fName, ios::binary);
+    int x;
+    cout << "\nfile \"" << fName << "\":" << endl;
+    while (f.read((char*)&x, sizeof(x)))
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+    f.close();
+}
+
+// Функція для розподілу чисел на парні та непарні у два окремі файли
+void Process(const char* fName, const char* evenName, const char* oddName)
+{
+    ifstream f(fName, ios::binary);
+    ofstream g(evenName, ios::binary); 
+    ofstream h(oddName, ios::binary); 
+    int x;
+    while (f.read((char*)&x, sizeof(x)))
+    {
+        if (x % 2 == 0)
+            g.write((char*)&x, sizeof(x));
+        else
+            h.write((char*)&x, sizeof(x));
+    }
+    f.close();
+    g.close();
+    h.close();
+}
+
+int main()
+{
+    char fname[100];
+    cout << "Enter main file name: "; cin >> fname;
+
+    Create(fname);
+    Print(fname);
+
+    Process(fname, "even.dat", "odd.dat");
+
+    Print("even.dat");
+    Print("odd.dat");
+
+    return 0;
+}
---
+++ Lab11.1/UnitTest11.1/UnitTest11.1.cpp
@@ -0,0 +1,45 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab11.1/Lab11.1.cpp" 
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace UnitTest111
+{
+	TEST_CLASS(UnitTest111)
+	{
+	public:
+
+		TEST_METHOD(TestProcessEvenOddDistribution)
+		{
+			char testIn[] = "test_in.dat";
+			char testEven[] = "test_even.dat";
+			char testOdd[] = "test_odd.dat";
+
+			ofstream f(testIn, ios::binary);
+			int n1 = 2, n2 = 3;
+			f.write((char*)&n1, sizeof(int));
+			f.write((char*)&n2, sizeof(int));
+			f.close();
+
+			Process(testIn, testEven, testOdd);
+
+			ifstream fEven(testEven, ios::binary);
+			int resEven;
+			fEven.read((char*)&resEven, sizeof(int));
+			Assert::AreEqual(2, resEven);
+			fEven.close();
+
+			ifstream fOdd(testOdd, ios::binary);
+			int resOdd;
+			fOdd.read((char*)&resOdd, sizeof(int));
+			Assert::AreEqual(3, resOdd); 
+			fOdd.close();
+
+			remove(testIn);
+			remove(testEven);
+			remove(testOdd);
+		}
+	};
+}
